1ff6b4d2db85c92b0fba97cfcee575915a70441f
hugosenari <hugosenari@gmail.com>
2022-03-12 03:23:47 -0300

    chore: re index taglib

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''TagLib Audio Meta-Data Library wrapper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-taglib-master.flake = false;
+  inputs.src-taglib-master.owner = "alex-laskin";
+  inputs.src-taglib-master.ref   = "refs/heads/master";
+  inputs.src-taglib-master.repo  = "nim-taglib";
+  inputs.src-taglib-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-taglib-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-taglib-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
